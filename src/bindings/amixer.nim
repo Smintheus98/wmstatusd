7d5901a67c83f92838cfc8b72695aa4b91dc14eb
Yannic Kitten <yannic.kitten@outlook.de>
2022-01-17 19:59:38 +0100

    Updated

--- src/bindings/amixer.nim
+++ src/bindings/amixer.nim
@@ -7,17 +7,6 @@ type Mixer* = object
   volmin, volmax: clong
   good*: bool
 
-proc init*(mixer: var Mixer; mixIdx = 0.cuint; mixName = "Master"; cardName = "default")
-proc initMixer*(mixIdx = 0.cuint; mixName = "Master"; cardName = "default"): Mixer
-
-proc `=destroy`(mixer: var Mixer)
-proc deinit*(mixer: var Mixer)
-
-proc update*(mixer: var Mixer): bool
-proc isMute*(mixer: Mixer; channel = SND_MIXER_SCHN_FRONT_LEFT): bool
-proc getVolume*(mixer: Mixer; channel = SND_MIXER_SCHN_FRONT_LEFT): int
-
-
 
 proc `=destroy`(mixer: var Mixer) =
   # TODO: Add some further free/close/unregister calls
@@ -36,7 +25,7 @@ proc deinit*(mixer: var Mixer) =
   mixer.`=destroy`
 
 
-proc init*(mixer: var Mixer; mixIdx: cuint; mixName, cardName: string) =
+proc init*(mixer: var Mixer; mixIdx = 0.cuint; mixName = "Master"; cardName = "default") =
   mixer.good = false
   if mixer.sid != nil:
     mixer.`=destroy`
@@ -55,16 +44,16 @@ proc init*(mixer: var Mixer; mixIdx: cuint; mixName, cardName: string) =
     mixer.`=destroy`
     return
 
-  if not mixer.update():
+  mixer.elem = snd_mixer_find_selem(mixer.handle, mixer.sid)
+  if mixer.elem == nil:
     mixer.`=destroy`
     return
-
 
   discard snd_mixer_selem_get_playback_volume_range(mixer.elem, addr mixer.volmin, addr mixer.volmax)
   mixer.good = true
 
 
-proc initMixer*(mixIdx = 0.cuint; mixName, cardName: string): Mixer =
+proc initMixer*(mixIdx = 0.cuint; mixName = "Master"; cardName = "default"): Mixer =
   init(result, mixIdx, mixName, cardName)
 
 
@@ -77,14 +66,14 @@ proc update*(mixer: var Mixer): bool =
   return true
 
 
-proc isMute*(mixer: Mixer; channel: snd_mixer_selem_channel_id): bool =
+proc isMuted*(mixer: Mixer; channel = SND_MIXER_SCHN_FRONT_LEFT): bool =
   var switchstate: cint
   if snd_mixer_selem_get_playback_switch(mixer.elem, channel, addr switchstate) < 0:
     return false
   return not switchstate.bool
 
 
-proc getVolume*(mixer: Mixer; channel: snd_mixer_selem_channel_id): int =
+proc getVolume*(mixer: Mixer; channel = SND_MIXER_SCHN_FRONT_LEFT): int =
   var volume: clong
   if snd_mixer_selem_get_playback_volume(mixer.elem, channel, addr volume) < 0:
     return -1
